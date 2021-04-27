d165a1939164c48ea3cf8ad2ff8a40e65ce4b687
jasnlha <jasnlha@icloud.com>
2021-04-27 13:50:58 -0700

    Add files via upload

---
+++ wav.h
@@ -0,0 +1,15 @@
+// Jason Ha
+
+class Wav{
+protected:
+    unsigned char* buffer = NULL;
+    wav_header waveHeader;
+    info info;
+    FMT fmt;
+    METADATA metaData;
+    DATA data;
+    MYSTERY mystery;
+    
+public:
+    
+}
