83a47bc1517acd915da955046f45a3722ac4737f
gyonano <gyonano77@nevada.unr.edu>
2021-04-29 11:13:04 -0700

    Finished noisegate.h

--- noisegate.h
+++ noisegate.h
@@ -1,7 +1,9 @@
 #ifndef NOISEGATE_H
 #define NOISEGATE_H
 
-#include <stdio.h>
+#include <string>
+#include <fstream>
+#include <iostream>
 #include <cstdint>
 #include <cmath>
 
@@ -9,14 +11,40 @@
 
 using namespace std; 
 
-class noiseGate : public Processor
+template <typename T>
+class noiseGate : public Processor<T>
 {
 	int threshold;
 
 public:
-	Noisegate(int threshold);
-	void processBuffer(unsigned char* buffer, int bufferSize);
+	NoiseGate(int threshold);
+	void processBuffer(T* buffer, int bufferSize);
 
 };
+template <typename T> NoiseGate <T>::NoiseGate(int threshold): threshold(threshold){}
+template <typename T> void NoiseGate <T>::processBuffer(T* buffer, int bufferSize)
+{
+	T zeroPoint;
+	zeroPoint = std:numberic_limits<T>.max();
+	for(int i = 0; i<bufferSize; i++)
+	{
+		if(buffer[i] == char)
+		{
+			if(buffer[i]<(zeroPoint+5) && (buffer[i] > (zeroPoint-5)))
+			{
+				buffer[i] = 128;
+			}
+		}
+		if(buffer[i] == short)
+		{
+			if(buffer[i]<(zeroPoint+5) && (buffer[i] > (zeroPoint-5)))
+			{
+				buffer[i] = 32768;
+			}
+		}
+		
+	}
+}
+
 #endif //NOISEGATE_H 
 
