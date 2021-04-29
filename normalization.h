06680039a2cb6ebc2b2d57c86b637231388711fb
gyonano <gyonano77@nevada.unr.edu>
2021-04-29 11:49:27 -0700

    Finished Normiliation .h and removed Normilization.cpp

--- normalization.h
+++ normalization.h
@@ -1,16 +1,41 @@
 #ifndef NORMALIZATION_H
 #define NORMALIZATION_H
 
+#include <iostream> 
 #include <cstdint>
 #include <cmath>
 
 #include "Processor.h"
 
-class Normalization: public Processor
+template <typename T>
+class Normalization: public Processor<T>
 {
+	int threshold;
 public:
-	void processBuffer(unsigned char* buffer, int bufferSize);
+	Normilizer(int threshold);
+	void processBuffer(T* buffer, int bufferSize);
 };
+template <typename T> Normilizer <T>::Normilizer(int threshold): threshold(threshold){}
+template <typename T> void Normilizer <T>::processBuffer(T* buffer, int bufferSize)
+{
+	int max = 0;
+	for(int i = 0; i<bufferSize; i++)
+	{
+		if(buffer[i]<max){
+			buffer[i] = max;
+		}
+		if(buffer[i] == char)
+		{
+			buffer[i] * (255/max);
+		}
+		if(buffer[i] == short)
+		{
+			buffer[i] * (max/32767);
+		}
+		
+	}
+}
+
 
 #endif //NORMALIZATION_H
 
