c0d0369b62de4e233faf82140c6409f1a816bedc
jasnlha <jasnlha@nevada.unr.edu>
2021-05-04 14:21:55 -0700

    Update wav.cpp
    updated with new code around how we read and write the files

--- wav.cpp
+++ wav.cpp
@@ -12,83 +12,83 @@ wavHeader Wav::getwavHeader(){
     return wave_Header;
 }
 
-unsigned char *Wav::getBuffer(){
-    return buffer;
+unsigned char *Wav::getBuffer(int i){
+    return buffers[i];
+}
+
+int Wav::getBufferSize() const{
+    return bufferSize_data;
+}
+
+int Wav::getBitDepth(){
+    return fmt.bit_depth;
+}
+
+int Wav::getNumChannels(){
+    return fmt.num_channels;
 }
 
 void Wav::readFile(const std::string &fileName){
     std::ifstream file(fileName, std::ios::binary | std::ios::in);
-    
     if(file.is_open()){
         file.read((char*)&wave_Header, sizeof(wavHeader));
-        while(file){
+
+        if (wave_Header.wav_size > 0) {
             file.read((char*)&data_Chunk, sizeof(dataChunk));
-            
-            std::string header_word = data_Chunk.fmt_header;
-            int chunkSize = data_Chunk.fmt_chunk_size;
-            
-            //Format Chunk
-            if(header_word == "FMT "){
-                file.read((char*)&fmt, sizeof(FMT));
-            }
-            //Metadata Chunk
-            else if(header_word == "LIST"){
-                int counter = 0;
-                char garbage[4];
-                file.read(garbage, 4);
-                while(counter < chunkSize){
-                    SubChunkData subchunk;
-                    file.read((char*)&subchunk, sizeof(dataChunk));
-                    subchunk.buffer = new char [subchunk.fmt_chunk_size];
-                    metadata.push_back(subchunk);
-                    counter += (sizeof(dataChunk) + subchunk.fmt_chunk_size);
-                }
-            }
-            //Data Chunk
-            else if(header_word == "DATA"){
-                bufferSize_data = chunkSize;
-                buffer = new unsigned char [chunkSize];
-                file.read((char*)&buffer, chunkSize);
-            }else{
-                //*seekg: allows you to seek to an arbitrary position in a file, it is used in file handling to sets the position of the next character to be extracted from the input stream from a given file.
-                file.seekg(chunkSize, std::ios::cur);
-            }
+        }
+
+        file.read((char*)&fmt, sizeof(FMT));
+
+        dataChunk chunk;
+        while (true) {
+            file.read((char*)&chunk, sizeof(dataChunk));
+            if (chunk.fmt_header[0] == 'd' && chunk.fmt_header[1] == 'a' && chunk.fmt_header[2] == 't' && chunk.fmt_header[3] == 'a') break;
+            file.seekg(chunk.fmt_chunk_size, std::ios::cur);
+        }
+
+        buffers = new unsigned char * [fmt.num_channels];
+        bufferSize_data = chunk.fmt_chunk_size;
+        int bytes_per_sample = fmt.bit_depth / 8;
+        for (int i = 0; i < fmt.num_channels; i++) {
+            buffers[i] = new unsigned char[bufferSize_data / fmt.num_channels];
+        }
+        
+        for (int i = 0; i < bufferSize_data; i++) {
+            file.read((char *) &buffers[i / bytes_per_sample % fmt.num_channels][i / fmt.num_channels], 1);
         }
     }
     file.close();
+    std::cout << "RIFF HEADER: " << wave_Header.riff_header << "\n";
+    std::cout << "WAV SIZE: " << wave_Header.wav_size << "\n";
+    std::cout << "WAV HEADER: " << wave_Header.wave_header << "\n";
+    std::cout << "FMT HEADER: " << data_Chunk.fmt_header << "\n";
+    std::cout << "FMT CHUNK SIZE: " << data_Chunk.fmt_chunk_size << "\n";
+    std::cout << "AUDIO FORMAT: " << fmt.audio_format << "\n";
+    std::cout << "NUM OF CHANNELS: " << fmt.num_channels << "\n";
+    std::cout << "SAMPLE RATE: " << fmt.sample_rate << "\n";
+    std::cout << "BYTE RATE: " << fmt.byte_rate << "\n";
+    std::cout << "SAMPLE ALLIGNMENT: " << fmt.sample_alignment << "\n";
+    std::cout << "BIT DEPTH: " << fmt.bit_depth << "\n";
 }
 
 void Wav::writeFile(const std::string &outFilename){
     std::ofstream outFile(outFilename, std::ios::out | std::ios::binary);
     outFile.write((char*)&wave_Header, sizeof(wavHeader));
-    
-    //Format Chunk
-    outFile.write("fmt ", 4);
-    int size = sizeof(fmt);
-    outFile.write((char*)&size, sizeof(size));
+    outFile.write((char*)&data_Chunk, sizeof(dataChunk));
     outFile.write((char*)&fmt, sizeof(FMT));
-    
-    //Metadata Chunk
-    outFile.write("LIST", 4);
-    size = 4;
-    for(SubChunkData s: metadata){
-        size += (sizeof(dataChunk) + s.fmt_chunk_size);
-    }
-    outFile.write((char*)&size, sizeof(size));
-    outFile.write("INFO", 4);
-    for(SubChunkData s: metadata){
-        outFile.write((char*)&s, sizeof(dataChunk));
-        outFile.write((char*)&s.buffer, sizeof(s.fmt_chunk_size));
-    }
-    
-    //Data Chunk
-    outFile.write("DATA", 4);
+    outFile.write("data", 4);
     outFile.write((char*)&bufferSize_data, sizeof(bufferSize_data));
-    outFile.write((char*)&buffer, bufferSize_data);
+    int bytes_per_sample = fmt.bit_depth / 8;
+    for (int i = 0; i < bufferSize_data; i++) {
+        outFile.write((char*)&(buffers[i / bytes_per_sample % fmt.num_channels][i / fmt.num_channels]), 1);
+    }
 }
 
 Wav::~Wav(){
-    if(buffer != NULL){
-        delete[] buffer;
+    if(buffers != NULL){
+        for (int i = 0; i < fmt.num_channels; i++) {
+            delete[] buffers[i];
+        }
     }
+    delete[] buffers;
 }
