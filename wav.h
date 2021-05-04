72329dc7cfe351bee1ede5e4061a8bab0b557a46
jasnlha <jasnlha@nevada.unr.edu>
2021-05-04 14:26:55 -0700

    Update wav.h
    made changes to file io

--- wav.h
+++ wav.h
@@ -2,6 +2,10 @@
 // CS 202: Semester Project
 // https://stackoverflow.com/questions/13660777/c-reading-the-data-part-of-a-wav-file
 
+/**
+ * This is the Wav class.
+*/
+
 #ifndef WAV_H
 #define WAV_H
 
@@ -10,58 +14,54 @@
 
 #include "wavheader.h"
 
-/**
- * This is the Wav class.
-*/
-
 class Wav{
 protected:
     int bufferSize_data;
 	//!< This variable .....
-
-    unsigned char* buffer = NULL;
+    unsigned char** buffers = NULL;
 	//!< This variable .....
-
     std::vector <SubChunkData> metadata;
 	//!< This variable .....
-
     wavHeader wave_Header;
 	//!< This variable .....
-
     dataChunk data_Chunk;
 	//!< This variable .....
-
     FMT fmt;
 	//!< This variable .....
-
 public:
-	/**
-	 * GetwavHeader ....
-	*/
     wavHeader getwavHeader();
-
 	/**
-	 * getBuffer ....
+	 * getBuffer....
 	*/
-    unsigned char *getBuffer();
-
+    unsigned char *getBuffer(int);
 	/**
-	 * GetbufferSize ....
+	 * getBufferSize....
 	*/
-    int getBufferSize();
-
+    int getBufferSize() const;
+	/**
+	 * getBitDepth....
+	*/
+    int getBitDepth();
+	/**
+	 * getNumChannels....
+	*/
+    int getNumChannels();
 	/**
 	 * Reads into the audio file.
 	 * @param filename - the name of the audio file
 	*/
     void readFile(const std::string &filename);
-
 	/**
 	 * Writes a new audio file.
 	 * @param filename - the name of the audio file
 	*/
     void writeFile(const std::string &outFilename);
-
+	/**
+	 * Prints out Metadata
+	 * @param filename - the name of the audio file
+	*/
+    void printMetaData();
+	
 	/**
 	 * Deconstructor of Wav class.
 	*/
